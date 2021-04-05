ca40355d77decbd22e7067dc7f800a022461360b
akhilalekha <akhila160@gmail.com>
2021-04-05 12:27:07 +0530

    modify UserItem component - modify styles of the user card - destructure this.state - add html url - create users component and an array users in the state

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -2,25 +2,39 @@ import React, { Component } from "react";
 import "./UserItem.css";
 
 export class UserItem extends Component {
-	constructor() {
-		super();
-		this.state = {
-			id: "",
-			login: "mojombo",
-			avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
-			html_url: "https://github.com/mojombo"
-		};
-	}
+	state = {
+		users: [
+			{
+				id: "",
+				login: "mojombo",
+				avatar_url: "https://avatars.githubusercontent.com/u/1?v=4",
+				html_url: "https://github.com/mojombo"
+			},
+			{
+				login: "defunkt",
+				id: 2,
+				avatar_url: "https://avatars.githubusercontent.com/u/2?v=4",
+				url: "https://api.github.com/users/defunkt",
+				html_url: "https://github.com/defunkt"
+			},
+			{
+				login: "pjhyett",
+				id: 3,
+				avatar_url: "https://avatars.githubusercontent.com/u/3?v=4",
+				url: "https://api.github.com/users/pjhyett",
+				html_url: "https://github.com/pjhyett"
+			}
+		]
+	};
 	render() {
+		const { avatar_url, login, html_url } = this.state;
 		return (
 			<div className="useritem">
-				<img
-					src={this.state.avatar_url}
-					alt="profile"
-					className="profile-img"
-				/>
-				<p>{this.state.login}</p>
-				<button>More</button>
+				<img src={avatar_url} alt="profile" className="profile-img" />
+				<h3>{login}</h3>
+				<div>
+					<a href={html_url}>More</a>
+				</div>
 			</div>
 		);
 	}
