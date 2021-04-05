36687c53a78215a8631bd79a93049e1a810f0731
akhilalekha <akhila160@gmail.com>
2021-04-05 15:11:50 +0530

    fetch data from GitHub api - create state for users and loading - pass these to the users component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import "./App.css";
 import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
-import User from "./components/users/Users";
+import Users from "./components/users/Users";
+import axios from "axios";
 
 class App extends Component {
+	state = {
+		users: [],
+		loading: false
+	};
+	async componentDidMount() {
+		this.setState({ loading: true });
+		const res = await axios.get("https://api.github.com/users");
+		console.log(res.data);
+		this.setState({ users: res.data, loading: true });
+	}
 	render() {
 		return (
 			<div className="App">
 				<Navbar />
-				<User />
+				<Users loading={this.state.loading} users={this.state.users} />
 			</div>
 		);
 	}
