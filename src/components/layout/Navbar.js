8bd04eab0324f776f872ac98386e9ec587669445
akhilalekha <akhila160@gmail.com>
2021-04-05 10:29:47 +0530

    create new component - create navbar - remove extra files in public - add styles for navbar - add fontawesome cdn script tag

---
+++ src/components/layout/Navbar.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Navbar.css";
+
+export default class Navbar extends Component {
+	render() {
+		return (
+			<nav>
+				<h1>Navbar</h1>
+			</nav>
+		);
+	}
+}
