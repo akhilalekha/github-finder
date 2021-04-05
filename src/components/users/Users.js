2b4f9df87f915c77ec90a952f9164e95112040fe
akhilalekha <akhila160@gmail.com>
2021-04-05 16:02:30 +0530

    add users and loading proptypes

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -2,6 +2,7 @@ import React from "react";
 import UserItem from "./UserItem";
 import "./Users.css";
 import Spinner from "../layout/Spinner";
+import PropTypes from "prop-types";
 
 const Users = ({ users, loading }) => {
 	if (loading) {
@@ -15,6 +16,11 @@ const Users = ({ users, loading }) => {
 			</div>
 		);
 	}
+};
+
+Users.propTypes = {
+	users: PropTypes.array.isRequired,
+	loading: PropTypes.bool.isRequired
 };
 
 export default Users;
