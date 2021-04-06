7bed302f87258fa864c7bc06b7b77e5593e39389
akhilalekha <akhila160@gmail.com>
2021-04-06 12:00:16 +0530

    create search users function - move api call to search users - change api url to /search/users - set users as res.data.items - add proptypes for function search users

--- src/App.js
+++ src/App.js
@@ -4,25 +4,38 @@ import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import axios from "axios";
 import Search from "./components/users/Search";
+import PropTypes from "prop-types";
 
 class App extends Component {
 	state = {
 		users: [],
 		loading: false
 	};
-	async componentDidMount() {
+
+	static propTypes = {
+		searchUsers: PropTypes.func.isRequired
+	};
+	// async componentDidMount() {
+	// 	this.setState({ loading: true });
+	// 	const res = await axios.get(
+	// 		`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+	// 	);
+	// 	this.setState({ users: res.data, loading: false });
+	// }
+
+	searchUsers = async text => {
 		this.setState({ loading: true });
 		const res = await axios.get(
-			`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
 		);
-		console.log(res.data);
-		this.setState({ users: res.data, loading: false });
-	}
+		// console.log(res.data);
+		this.setState({ users: res.data.items, loading: false });
+	};
 	render() {
 		return (
 			<div className="App">
 				<Navbar />
-				<Search />
+				<Search searchUsers={this.searchUsers} />
 				<Users loading={this.state.loading} users={this.state.users} />
 			</div>
 		);
--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -2,15 +2,27 @@ import React, { Component } from "react";
 import "./Search.css";
 
 export class Search extends Component {
+	state = {
+		text: ""
+	};
+
+	onChange = e => this.setState({ [e.target.name]: e.target.value });
+	onSubmit = e => {
+		e.preventDefault();
+		this.props.searchUsers(this.state.text);
+		this.setState({ text: "" });
+	};
 	render() {
 		return (
 			<div className="search">
-				<form className="form">
+				<form onSubmit={this.onSubmit} className="form">
 					<input
 						type="text"
 						name="text"
 						placeholder="Search Users..."
 						className="search-inp"
+						value={this.state.text}
+						onChange={this.onChange}
 					/>
 					<input type="submit" value="Search" className="search-btn" />
 				</form>
